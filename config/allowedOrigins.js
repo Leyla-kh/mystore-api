0839a4d78ec77c6f0debe7986db3eb03958f41e0
Leyla-kh <98767820+Leyla-kh@users.noreply.github.com>
2023-09-09 22:12:41 +0330

    new edit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://e-commerc-zm9y.onrender.com"];
+const allowedOrigins = [
+  "https://e-commerc-zm9y.onrender.com",
+  "http://localhost:3000",
+];
 
 module.exports = allowedOrigins;
